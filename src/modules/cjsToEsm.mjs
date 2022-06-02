19ab7041d11f253ef2937d709c173b2d8345a312
Igor Borovoy <igorborovoy@yandex.by>
2022-06-03 00:05:06 +0300

    feat: implement modules task

---
+++ src/modules/cjsToEsm.mjs
@@ -0,0 +1,28 @@
+import path from 'path';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/a.json' assert { type: 'json'};
+import './files/b.json' assert { type: 'json'};
+import './files/c.js';
+
+const random = Math.random();
+
+export const unknownObject = random > 0.5
+    ? JSON.parse(await readFile('./files/a.json'))
+    : JSON.parse(await readFile('./files/b.json'));
+
+const fileName = fileURLToPath(import.meta.url);
+const dirPath = path.dirname(fileName);
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${fileName}`);
+console.log(`Path to current directory is ${dirPath}`);
+
+export const createMyServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
