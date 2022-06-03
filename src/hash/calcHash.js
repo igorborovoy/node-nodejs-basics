992ac27d25d3aa69e387cc5271e107221d18682e
Igor Borovoy <igorborovoy@yandex.by>
2022-06-03 15:14:22 +0300

    feat: implement crypto task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,12 @@
+import { readFile } from 'fs';
+import crypto from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const file = readFile('./files/fileToCalculateHashFor.txt', (err, data) => {
+        if (err) throw new Error('FS operation failed');
+        const hash = crypto.createHash('sha256').update(data).digest('hex');
+        console.log(hash)
+    });
 };
+
+calculateHash();
