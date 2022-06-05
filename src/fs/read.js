2106b7274656da946b9cc058702c56e8187fd02a
Igor Borovoy <igorborovoy@yandex.by>
2022-06-05 22:10:38 +0300

    fix: add script, fix path

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,9 +1,11 @@
 import { readFile } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export const read = async () => {
-     const filePath = './files/fileToRead.txt';
-
-     readFile(filePath, {encoding: 'utf-8'}, (err, buff) => {
+     const __filename = fileURLToPath(import.meta.url)
+     const __dirname = path.dirname(__filename);
+     readFile(__dirname + '/files/fileToRead.txt', {encoding: 'utf-8'}, (err, buff) => {
          if (err) throw new Error('FS operation failed');
          console.log(buff);
      });
