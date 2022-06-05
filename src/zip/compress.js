720c118118a5bfde3a09767e28d4b338857aadf7
Igor Borovoy <igorborovoy@yandex.by>
2022-06-05 20:00:49 +0300

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./files/fileToCompress.txt', 'utf-8');
+    const writeStream = createWriteStream('./files/archive.gz');
+    const gzip = createGzip();
+
+    pipeline(
+        readStream,
+        gzip,
+        writeStream,
+        err => new Error(err)
+    );
 };
+
+compress();
