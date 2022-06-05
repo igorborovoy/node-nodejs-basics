b329f27287072cb7b43f5ab7ec4a8b9e4ec98a55
Igor Borovoy <igorborovoy@yandex.by>
2022-06-05 20:57:54 +0300

    feat: implement workers tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,26 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const numberOfCpus = cpus().length;
+    let n = 10;
+    const promises = [];
+    
+    for(let i = 0; i < numberOfCpus; i++) {
+        promises.push(createWorkerPromise(n));
+        n++;
+    }
+
+    function createWorkerPromise(n) {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', {workerData: n});
+            worker.on('message', (data) => resolve(data));
+            worker.on('error', (err) => reject(err))
+        });
+    }
+    
+    Promise.allSettled(promises).then((data) => console.log(data));
+    
 };
+
+performCalculations();
